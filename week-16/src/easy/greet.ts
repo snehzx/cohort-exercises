9e71725f434bf2633153f697e3828acc13a6eb02
snehzx <24CSEAIML075@jssaten.ac.in>
2026-05-22 22:43:06 +0530

    week-16

---
+++ week-16/src/easy/greet.ts
@@ -0,0 +1,4 @@
+// Write a function that takes a user's full name as input (a string) and returns a greeting with their initials.
+// Use a type annotation for the input and output.
+// Example Input: "Pixy Glee"
+// Example Output: "Hello, P.G!"
