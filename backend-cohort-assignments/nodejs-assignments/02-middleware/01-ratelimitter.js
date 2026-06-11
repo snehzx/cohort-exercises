005daed2f0b77032b7e6c6be3babf1a382bec83e
snehzx <24CSEAIML075@jssaten.ac.in>
2026-06-11 23:57:24 +0530

    middleware assign done

--- backend-cohort-assignments/nodejs-assignments/02-middleware/01-ratelimitter.js
+++ backend-cohort-assignments/nodejs-assignments/02-middleware/01-ratelimitter.js
@@ -1,6 +1,6 @@
 // You have to create a middleware for rate limiting a users request based on their username passed in the header
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // Your task is to create a global middleware (app.use) which will
@@ -10,18 +10,43 @@ const app = express();
 // User will be sending in their user id in the header as 'user-id'
 // You have been given a numberOfRequestsForUser object to start off with which
 // clears every one second
-
 let numberOfRequestsForUser = {};
-setInterval(() => {
-    numberOfRequestsForUser = {};
-}, 1000)
-
-app.get('/user', function(req, res) {
-  res.status(200).json({ name: 'john' });
+app.use((req, res, next) => {
+  const userId = req.headers["user-id"];
+  if (!userId) {
+    return res.status(404).json({
+      message: "userId not found",
+    });
+  }
+  let currTime = Math.floor(Date.now() / 1000);
+  if (!numberOfRequestsForUser[userId]) {
+    numberOfRequestsForUser[userId] = { count: 0, reqTime: currTime };
+  }
+  const userData = numberOfRequestsForUser[userId];
+  if (currTime === userData.reqTime) {
+    userData.count += 1;
+  } else {
+    userData.count = 1;
+    userData.reqTime = currTime;
+  }
+  if (userData.count > 5) {
+    return res.status(404).json({
+      error: "too many requests",
+    });
+  }
+  next();
 });
 
-app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
+setInterval(() => {
+  numberOfRequestsForUser = {};
+}, 1000);
+
+app.get("/user", function (req, res) {
+  res.status(200).json({ name: "john" });
+});
+
+app.post("/user", function (req, res) {
+  res.status(200).json({ msg: "created dummy user" });
 });
 
 module.exports = app;
